98da1a6f6566a44c4ee5755dad4ae7ddd882a980
harryd13 <35573650+harryd13@users.noreply.github.com>
2019-10-27 12:40:03 +0530

    Add files via upload

---
+++ checkUsername.cpp
@@ -0,0 +1,10 @@
+#include<iostream> 
+#include<stdio.h>
+#include<string> // for string class 
+#include<string.h>
+#include<vector>
+#include <my_global.h>
+#include <mysql.h>
+using namespace std;
+
+int check(mysql_)
